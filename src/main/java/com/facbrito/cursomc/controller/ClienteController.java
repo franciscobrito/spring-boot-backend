bdc725573764fe00e2719208b77366ff39982f31
Francisco Brito <francisco.brito.ac@gmail.com>
2021-01-03 23:35:25 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/facbrito/cursomc/controller/ClienteController.java
+++ src/main/java/com/facbrito/cursomc/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.facbrito.cursomc.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,14 +12,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.facbrito.cursomc.domain.Cliente;
 import com.facbrito.cursomc.dto.ClienteDTO;
+import com.facbrito.cursomc.dto.ClienteNewDTO;
 import com.facbrito.cursomc.service.ClienteService;
 
 @RestController
@@ -32,6 +36,15 @@ public class ClienteController {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente cliente = clienteService.find(id);
 		return ResponseEntity.ok().body(cliente);
+	}
+
+	@PostMapping
+	public ResponseEntity<?> insert(@Valid @RequestBody ClienteNewDTO clienteNewDTO) {
+		Cliente cliente = clienteService.fromDTO(clienteNewDTO);
+		cliente = clienteService.insert(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 	@PutMapping(value = "/{id}")
--- src/main/java/com/facbrito/cursomc/service/ClienteService.java
+++ src/main/java/com/facbrito/cursomc/service/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.facbrito.cursomc.domain.Cidade;
 import com.facbrito.cursomc.domain.Cliente;
+import com.facbrito.cursomc.domain.Endereco;
+import com.facbrito.cursomc.domain.enums.TipoCliente;
 import com.facbrito.cursomc.dto.ClienteDTO;
+import com.facbrito.cursomc.dto.ClienteNewDTO;
 import com.facbrito.cursomc.repository.ClienteRepository;
+import com.facbrito.cursomc.repository.EnderecoRepository;
 import com.facbrito.cursomc.service.exception.DataIntegrityException;
 import com.facbrito.cursomc.service.exception.ObjectNotFoundException;
 
@@ -22,10 +28,21 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository clienteRepository;
 
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> cliente = clienteRepository.findById(id);
 		return cliente.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontra! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	@Transactional
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(cliente.getId());
+		cliente = clienteRepository.save(cliente);
+		enderecoRepository.saveAll(cliente.getEnderecos());
+		return cliente;
 	}
 
 	public Cliente update(Cliente cliente) {
@@ -51,9 +68,30 @@ public class ClienteService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return clienteRepository.findAll(pageRequest);
 	}
+	
+	public Cliente fromDTO(ClienteDTO cliente) {
+		return new Cliente(cliente.getId(), cliente.getNome(), cliente.getEmail(), null, null);
+	}
 
-	public Cliente fromDTO(ClienteDTO clienteDTO) {
-		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	public Cliente fromDTO(ClienteNewDTO clienteNewDTO) {
+		Cliente cliente = new Cliente(null, clienteNewDTO.getNome(), clienteNewDTO.getEmail(),
+				clienteNewDTO.getCpfOuCnpj(), TipoCliente.toEnum(clienteNewDTO.getTipo()));
+
+		Cidade cidade = new Cidade(clienteNewDTO.getCidadeId(), null, null);
+
+		Endereco endereco = new Endereco(null, clienteNewDTO.getLogradouro(), clienteNewDTO.getNumero(),
+				clienteNewDTO.getComplemento(), clienteNewDTO.getBairro(), clienteNewDTO.getCep(), cliente, cidade);
+
+		cliente.getEnderecos().add(endereco);
+		cliente.getTelefones().add(clienteNewDTO.getTelefone1());
+
+		if (clienteNewDTO.getTelefone2() != null) {
+			cliente.getTelefones().add(clienteNewDTO.getTelefone2());
+		}
+		if (clienteNewDTO.getTelefone3() != null) {
+			cliente.getTelefones().add(clienteNewDTO.getTelefone3());
+		}
+		return cliente;
 	}
 
 	private void updateData(Cliente newCliente, Cliente cliente) {
